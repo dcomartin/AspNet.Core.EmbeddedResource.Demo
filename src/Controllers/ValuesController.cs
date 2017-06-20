4ad2a69405824b4b9ed0f018c077102193a0f8a8
Derek Comartin <dcomartin@gmail.com>
2017-06-19 21:01:37 -0400

    Initial demo code

---
+++ src/Controllers/ValuesController.cs
@@ -0,0 +1,24 @@
+﻿using System.IO;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmbeddedResource.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+        // GET api/values
+        [HttpGet, Produces("application/json")]
+        public async Task<string> Get()
+        {
+            var assembly = Assembly.GetEntryAssembly();
+            var resourceStream = assembly.GetManifestResourceStream("EmbeddedResource.Data.fakedata.json");
+            using (var reader = new StreamReader(resourceStream, Encoding.UTF8))
+            {
+                return await reader.ReadToEndAsync();
+            }
+        }
+    }
+}
